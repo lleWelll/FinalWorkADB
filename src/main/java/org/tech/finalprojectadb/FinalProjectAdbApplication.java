1f14db2460db2dcb12368606e3027202251a16c6
llewell <llwll@MacBoo.local>
2025-11-02 14:23:04 +0500

    initial commit

---
+++ src/main/java/org/tech/finalprojectadb/FinalProjectAdbApplication.java
@@ -0,0 +1,13 @@
+package org.tech.finalprojectadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectAdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectAdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tech/finalprojectadb/FinalProjectAdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tech.finalprojectadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectAdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
